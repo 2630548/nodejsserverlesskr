6f4ef104a3f183547359932895791ade8dcecc65
2630548 <130237209+2630548@users.noreply.github.com>
2025-06-04 09:59:18 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
-// api/fetch.js
-export default async function handler(req, res) {
+const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
   try {
-    const response = await fetch('https://www.google.com');
+    const response = await fetch('http://www.google.com');
     const data = await response.text();
-    res.status(200).json({ data });
+    res.status(200).send(data);
   } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch data' });
+    res.status(500).send('Error fetching example.com: ' + error.message);
   }
-}
+};
