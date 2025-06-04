e8ae007027a896a538bc0b700718ab773edc06a2
2630548 <130237209+2630548@users.noreply.github.com>
2025-06-04 10:08:28 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,14 @@ module.exports = async (req, res) => {
   try {
     const response = await fetch('https://www.google.com');
     const data = await response.text();
-    res.status(200).send(data);
+     // 将网页内容转换为 base64
+    const base64Data = Buffer.from(data).toString('base64');
+    
+    // 设置响应头
+    res.status(200).json({
+      success: true,
+      data: base64Data
+    });
   } catch (error) {
     res.status(500).send('Error fetching example.com: ' + error.message);
   }
