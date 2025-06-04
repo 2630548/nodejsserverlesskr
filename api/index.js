76d936d0ce76f337028cce19a3631d76040d60d2
2630548 <130237209+2630548@users.noreply.github.com>
2025-06-04 10:00:31 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
   try {
-    const response = await fetch('http://www.google.com');
+    const response = await fetch('https://www.google.com');
     const data = await response.text();
     res.status(200).send(data);
   } catch (error) {
